58404ea35381b4c39bf69e55f8f5a8218a199408
VJRK <VJRK@protonmail.com>
2020-06-29 14:09:56 +0200

    Game Loop und Fenster

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+import pygame
+
+# Fenster
+window = pygame.display.set_mode((1600, 900), flags=pygame.RESIZABLE)
+pygame.display.set_caption("Currentflow")
+
+# Game loop
+clock = pygame.time.Clock()
+max_fps = 60
+dt = clock.tick(max_fps)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+
+    # Render
+    window.fill((0, 0, 0))
+
+    # Canvas updaten
+    pygame.display.update()
+
+    # Ende Game loop
+    clock.tick(max_fps)
